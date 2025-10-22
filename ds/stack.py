263cb8edc80842af9ed1ccb3cbecd1b81bf814c2
Jeremy A Gray <grayj2@wcslive.com>
2025-10-22 14:20:10 -0500

    feat(ds): create stack and queue interfaces
    Signed-off-by: Jeremy A Gray <grayj2@wcslive.com>

--- ds/stack.py
+++ ds/stack.py
@@ -18,16 +18,61 @@ from .node import DoublyLinkedNode  # noqa:  F401
 class Stack:
     """Represent a stack."""
 
-    def __init__(self):
-        """Initialize a ``Stack``."""
-        pass
+    def __init__(self, *args):
+        """Initialize the ``Stack``.
+
+        Initialize the ``Stack``.  Push any items in ``args`` to the
+        stack in order.
+
+        Parameters
+        ----------
+        args : list
+            Items to append to the stack.
+
+        """
+        self.head = None
+        self.tail = None
+
+        for item in args:
+            self.push(item)
 
     def __repr__(self):
-        """Reproduce a ``Stack``."""
-        pass
+        """Reproduce the ``Stack``."""
+        return "Stack(" + ", ".join([f"'{item!s}'" for item in self]) + ")"
 
     def __str__(self):
-        """Stringify a ``Stack``."""
+        """Stringify the ``Stack``."""
+        return "[" + ", ".join([f"'{item!s}'" for item in self]) + "]"
+
+    def __len__(self):
+        """Find the length of the ``Stack``.
+
+        Returns
+        -------
+        int
+            The length of the stack.
+
+        """
+        pass
+
+    def __iter__(self):
+        """Iterate over the ``Stack``.
+
+        Iterate over the stack from head to tail by popping to enable
+        iterator protocol.
+
+        """
+        pass
+
+    def isEmpty(self):
+        """Determine if the ``Stack`` is empty.
+
+        Returns
+        -------
+        bool
+            ``True`` if the ``Stack`` is empty, ``False`` otherwise.
+
+        """
         pass
 
     def push(self, record):
