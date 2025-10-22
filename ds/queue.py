d8a7e954bce628e4efc31c9427950504b5fa4cbf
Jeremy A Gray <grayj2@wcslive.com>
2025-10-22 14:20:10 -0500

    feat(ds): create stack and queue interfaces
    Signed-off-by: Jeremy A Gray <grayj2@wcslive.com>

--- ds/queue.py
+++ ds/queue.py
@@ -18,16 +18,61 @@ from .node import DoublyLinkedNode  # noqa:  F401
 class Queue:
     """Represent a queue."""
 
-    def __init__(self):
-        """Initialize a ``Queue``."""
-        pass
+    def __init__(self, *args):
+        """Initialize the ``Queue``.
+
+        Initialize the ``Queue``.  Enqueue any items in ``args`` to
+        the queue in order.
+
+        Parameters
+        ----------
+        args : list
+            Items to enqueue onto the queue.
+
+        """
+        self.head = None
+        self.tail = None
+
+        for item in args:
+            self.enqueue(item)
 
     def __repr__(self):
-        """Reproduce a ``Queue``."""
-        pass
+        """Reproduce the ``Queue``."""
+        return "Queue(" + ", ".join([f"'{item!s}'" for item in self]) + ")"
 
     def __str__(self):
-        """Stringify a ``Queue``."""
+        """Stringify the ``Queue``."""
+        return "[" + ", ".join([f"'{item!s}'" for item in self]) + "]"
+
+    def __len__(self):
+        """Find the length of the ``Queue``.
+
+        Returns
+        -------
+        int
+            The length of the queue.
+
+        """
+        pass
+
+    def __iter__(self):
+        """Iterate over the ``Queue``.
+
+        Iterate over the queue from head to tail by dequeuing to
+        enable iterator protocol.
+
+        """
+        pass
+
+    def isEmpty(self):
+        """Determine if the ``Queue`` is empty.
+
+        Returns
+        -------
+        bool
+            ``True`` if the ``Queue`` is empty, ``False`` otherwise.
+
+        """
         pass
 
     def enqueue(self, record):
