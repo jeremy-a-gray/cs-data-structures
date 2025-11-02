5e7b8a49ecbfdad4b6461495e237edc5d0d2688a
Jeremy A Gray <grayj2@wcslive.com>
2025-11-02 15:50:50 -0600

    feat(ds): implement doubly linked lists
    Signed-off-by: Jeremy A Gray <grayj2@wcslive.com>

--- ds/double.py
+++ ds/double.py
@@ -12,20 +12,331 @@
 
 """Doubly-Linked Lists."""
 
-from .node import DoublyLinkedNode  # noqa:  F401
+from .node import DoublyLinkedNode
 
 
 class DoublyLinkedList:
     """Represent a doubly-linked list."""
 
-    def __init__(self):
-        """Initialize a ``DoublyLinkedList``."""
-        pass
+    def __init__(self, *args):
+        """Initialize the ``DoublyLinkedList``.
+
+        Initialize the ``DoublyLinkedList``.  Append any items in
+        ``args`` to the list in order.
+
+        Parameters
+        ----------
+        args : list
+            Items to append to the list.
+
+        """
+        self.head = None
+        self.tail = None
+
+        for item in args:
+            self.append(item)
 
     def __repr__(self):
-        """Reproduce a ``DoublyLinkedList``."""
-        pass
+        """Reproduce the ``DoublyLinkedList``."""
+        return "DoublyLinkedList(" + ", ".join([f"'{item!s}'" for item in self]) + ")"
 
     def __str__(self):
-        """Stringify a ``DoublyLinkedList``."""
-        pass
+        """Stringify the ``DoublyLinkedList``."""
+        return "[" + ", ".join([f"'{item!s}'" for item in self]) + "]"
+
+    def __len__(self):
+        """Find the length of the list.
+
+        Returns
+        -------
+        int
+            The length of the list.
+
+        """
+        # Empty list.
+        if self.isEmpty():
+            return 0
+
+        # Traverse the list.
+        cur = self.head
+        length = 0
+
+        while cur is not None:
+            cur = cur.next
+            length += 1
+
+        return length
+
+    def __iter__(self):
+        """Iterate over the list."""
+        cur = self.head
+
+        while cur is not None:
+            yield cur
+            cur = cur.next
+
+    def isEmpty(self):
+        """Determine if the list is empty.
+
+        Returns
+        -------
+        bool
+            ``True`` if the list is empty, ``False`` otherwise.
+
+        """
+        return True if self.head is None else False
+
+    def reverse(self):
+        """Reverse the list."""
+        # Empty list.
+        if self.isEmpty():
+            return
+
+        next = None
+        cur = self.head
+
+        # Iterate over list.
+        while cur is not None:
+            next = cur.next
+            cur.next = cur.prev
+            cur.prev = next
+            cur = next
+
+        # Reset head and tail.
+        tmp = self.head
+        self.head = self.tail
+        self.tail = tmp
+
+        return
+
+    def index(self, record):
+        """Find the index of the matching ``record`` in the list.
+
+        Returns
+        -------
+        int
+            The zero-based index of the node matching ``record`` in
+            the list.  Returns -1 if ``record`` is not found.
+
+        """
+        # Traverse the list.
+        ind = 0
+        cur = self.head
+
+        # Iterate until there is no next.
+        while cur is not None:
+            if cur.record == record:
+                return ind
+
+            cur = cur.next
+            ind += 1
+
+        return -1
+
+    def find_head(self):
+        """Find the head the list.
+
+        Returns
+        -------
+        DoublyLinkedNode
+            The head of the list.
+
+        """
+        return self.head
+
+    def find_tail(self):
+        """Find the tail the list.
+
+        Returns
+        -------
+        DoublyLinkedNode
+            The tail of the list.
+
+        """
+        return self.tail
+
+    def insert_before(self, new, record=None):
+        """Insert a node into the list.
+
+        Insert a new node with record ``new`` into the list before the
+        node with record ``record``.  If no record is given, then
+        prepend the new node onto the list.  If no record is found,
+        then append the new node onto the list.
+
+        """
+        # Build the node.
+        node = DoublyLinkedNode(new)
+
+        # Empty list.
+        if self.isEmpty():
+            self.append(new)
+
+            return
+
+        # Prepend on no record.
+        if record is None:
+            node.prev = None
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
+
+            return
+
+        # Traverse the list.
+        cur = self.head
+
+        while cur is not None:
+            if cur.record == record:
+                # Prepend the node.
+                if cur == self.head:
+                    node.prev = None
+                    self.head.prev = node
+                    node.next = self.head
+                    self.head = node
+                else:
+                    node.prev = cur.prev
+                    node.next = cur
+                    cur.prev.next = node
+                    cur.prev = node
+
+                return
+
+            cur = cur.next
+
+        # Append if no match.
+        self.append(new)
+
+        return
+
+    def insert_after(self, new, record=None):
+        """Insert a node in the list.
+
+        Insert a new node with record ``new`` into the list after the
+        node with record ``record``.  If no record is given, then
+        append the new node onto the list.  If no record is found,
+        then append the new node onto the list.
+
+        """
+        # Build the node.
+        node = DoublyLinkedNode(new)
+
+        # Empty list.
+        if self.isEmpty():
+            self.append(new)
+
+            return
+
+        # Traverse the list.
+        cur = self.head
+
+        while cur is not None:
+            if cur.record == record:
+                # Append the node.
+                if cur == self.tail:
+                    self.append(new)
+                else:
+                    node.prev = cur
+                    node.next = cur.next
+                    cur.next.prev = node
+                    cur.next = node
+
+                return
+
+            cur = cur.next
+
+        # Append if no match.
+        self.append(new)
+
+        return
+
+    def append(self, record):
+        """Append ``record`` onto the end of the list.
+
+        Parameters
+        ----------
+        record
+            The value to be appended to the list.
+
+        """
+        # Build the node.
+        node = DoublyLinkedNode(record)
+
+        # Empty list.
+        if self.isEmpty():
+            self.head = node
+            self.tail = node
+            self.head.next = None
+            self.head.prev = None
+            self.tail.next = None
+            self.tail.prev = None
+
+            return
+
+        # Non-empty list; append the node and fix the tail.
+        self.tail.next = node
+        node.prev = self.tail
+        node.next = None
+        self.tail = node
+
+        return
+
+    def delete(self, record):
+        """Remove the node matching ``record`` from the list.
+
+        Returns
+        -------
+        record
+            The ``record`` from the removed node or ``None`` if no
+            node was removed.  Removes the first node matching
+            ``record``.
+
+        """
+        # Traverse the list.
+        cur = self.head
+
+        while cur is not None:
+            if cur.record == record:
+                # Delete the node.
+                if cur == self.head:
+                    self.head = self.head.next
+                    self.head.prev = None
+                elif cur == self.tail:
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+                else:
+                    cur.prev.next = cur.next
+                    cur.next.prev = cur.prev
+
+                # Return the match.
+                return record
+
+            cur = cur.next
+
+        # No match.
+        return None
+
+    def pop(self):
+        """Remove the tail node from the list.
+
+        Returns
+        -------
+        record
+            The record of the removed node or ``None`` if empty.
+
+        """
+        # Empty list.
+        if self.isEmpty():
+            return None
+
+        # Pop the node and fix the tail.
+        node = self.tail
+
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail.prev.next = None
+            self.tail = self.tail.prev
+
+        # Return former tail.
+        return node.record
--- tests/test_doubly_linked.py
+++ tests/test_doubly_linked.py
@@ -11,3 +11,545 @@
 # ******************************************************************************
 
 """Doubly-linked list tests."""
+
+from ds import DoublyLinkedList
+
+
+def test_should_stringify_empty_list():
+    """Should stringify an empty list."""
+    a = DoublyLinkedList()
+
+    assert str(a) == "[]"
+
+
+def test_should_stringify_one_item_list():
+    """Should stringify a one item list."""
+    a = DoublyLinkedList("one")
+
+    assert str(a) == "['one']"
+
+
+def test_should_stringify_many_item_list():
+    """Should stringify a many item list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert str(a) == "['one', 'two', 'three']"
+
+
+def test_should_reproduce_empty_list():
+    """Should reproduce an empty list."""
+    a = DoublyLinkedList()
+
+    assert repr(a) == "DoublyLinkedList()"
+
+
+def test_should_reproduce_one_item_list():
+    """Should reproduce a one item list."""
+    a = DoublyLinkedList("one")
+
+    assert repr(a) == "DoublyLinkedList('one')"
+
+
+def test_should_reproduce_many_item_list():
+    """Should reproduce a many item list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert repr(a) == "DoublyLinkedList('one', 'two', 'three')"
+
+
+def test_should_find_head_of_empty_list():
+    """Should find the head of an empty list."""
+    a = DoublyLinkedList()
+
+    assert a.find_head() is None
+
+
+def test_should_find_head_of_nonempty_list():
+    """Should find the head of a non-empty list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert a.find_head().record == "one"
+    assert a.find_head().prev is None
+    assert a.find_head().next.record == "two"
+
+
+def test_should_find_tail_of_empty_list():
+    """Should find the tail of an empty list."""
+    a = DoublyLinkedList()
+
+    assert a.find_tail() is None
+
+
+def test_should_find_tail_of_nonempty_list():
+    """Should find the tail of a non-empty list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert a.find_tail().record == "three"
+    assert a.find_tail().next is None
+    assert a.find_tail().prev.record == "two"
+
+
+def test_should_find_len_of_empty_list():
+    """Should find the length of an empty list."""
+    a = DoublyLinkedList()
+
+    assert len(a) == 0
+
+
+def test_should_find_len_of_nonempty_list():
+    """Should find the length of a non-empty list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert len(a) == 3
+
+
+def test_should_append_empty():
+    """Should append to an empty list."""
+    record = "one"
+
+    a = DoublyLinkedList()
+
+    a.append(record)
+
+    assert len(a) == 1
+    assert a.head.record == record
+    assert a.tail.record == record
+    assert a.head == a.tail
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.tail.prev is None
+    assert a.tail.next is None
+
+
+def test_should_append_non_empty():
+    """Should append to a non-empty list."""
+    a = DoublyLinkedList("one", "two")
+
+    a.append("three")
+
+    assert len(a) == 3
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.tail.record == "three"
+    assert a.tail.prev.record == "two"
+    assert a.tail.next is None
+
+
+def test_should_implement_iter_empty():
+    """Should implement iterator protocol for an empty list."""
+    a = DoublyLinkedList()
+
+    assert [item.record for item in a] == []
+
+
+def test_should_implement_iter_nonempty():
+    """Should implement iterator protocol for a non-empty list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert [item.record for item in a] == ["one", "two", "three"]
+
+
+def test_should_index_empty():
+    """Should index an empty list."""
+    a = DoublyLinkedList()
+
+    assert a.index("one") == -1
+
+
+def test_should_index_nonempty_match():
+    """Should index a non-empty list with a match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert a.index("one") == 0
+    assert a.index("two") == 1
+    assert a.index("three") == 2
+
+
+def test_should_index_nonempty_no_match():
+    """Should index a non-empty list without a match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert a.index("four") == -1
+
+
+def test_should_pop_empty():
+    """Should pop an empty list."""
+    a = DoublyLinkedList()
+
+    assert a.pop() is None
+    assert len(a) == 0
+    assert a.head is None
+    assert a.tail is None
+
+
+def test_should_pop_nonempty_len_one():
+    """Should pop a non-empty, length one list."""
+    a = DoublyLinkedList("one")
+
+    tail = a.pop()
+    assert tail == "one"
+    assert len(a) == 0
+    assert a.head is None
+    assert a.tail is None
+
+
+def test_should_pop_nonempty_len_two():
+    """Should pop a non-empty, length two list."""
+    a = DoublyLinkedList("one", "two")
+
+    tail = a.pop()
+    assert tail == "two"
+    assert len(a) == 1
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.tail.record == "one"
+    assert a.tail.prev is None
+    assert a.tail.next is None
+    assert a.head == a.tail
+
+
+def test_should_pop_nonempty_len_three():
+    """Should pop a non-empty, length three list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    tail = a.pop()
+    assert tail == "three"
+    assert len(a) == 2
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next == a.tail
+    assert a.tail.record == "two"
+    assert a.tail.prev == a.head
+    assert a.tail.next is None
+
+
+def test_should_delete_empty():
+    """Should delete an empty list."""
+    a = DoublyLinkedList()
+
+    assert a.delete("one") is None
+    assert len(a) == 0
+    assert a.head is None
+    assert a.tail is None
+
+
+def test_should_delete_nonempty_match_head():
+    """Should delete a non-empty list with a head match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    record = a.delete("one")
+    assert record == "one"
+    assert len(a) == 2
+    assert a.head.record == "two"
+    assert a.head.prev is None
+    assert a.head.next == a.tail
+    assert a.head == a.tail.prev
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_delete_nonempty_match_middle():
+    """Should delete a non-empty list with a middle match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    record = a.delete("two")
+    assert record == "two"
+    assert len(a) == 2
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next == a.tail
+    assert a.head == a.tail.prev
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_delete_nonempty_match_tail():
+    """Should delete a non-empty list with a tail match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    record = a.delete("three")
+    assert record == "three"
+    assert len(a) == 2
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next == a.tail
+    assert a.head == a.tail.prev
+    assert a.tail.record == "two"
+    assert a.tail.next is None
+
+
+def test_should_delete_nonempty_no_match():
+    """Should delete a non-empty list without a match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    assert a.delete("four") is None
+    assert len(a) == 3
+    assert a.head.prev is None
+    assert a.head.record == "one"
+    assert a.head.next.record == "two"
+    assert a.head.next.next == a.tail
+    assert a.tail.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_insert_after_empty():
+    """Should insert after with an empty list."""
+    a = DoublyLinkedList()
+    a.insert_after("one", "zero")
+
+    assert len(a) == 1
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.head == a.tail
+
+
+def test_should_insert_after_nonempty_match_head():
+    """Should insert after on a non-empty list with a head match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_after("zero", "one")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "zero"
+    assert a.head.next.prev == a.head
+    assert a.head.next.next.record == "two"
+    assert a.head.next.next.prev.prev == a.head
+    assert a.head.next.next.next == a.tail
+    assert a.head.next.next.next.prev.prev.prev == a.head
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_insert_after_nonempty_match_middle():
+    """Should insert after on a non-empty list with a middle match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_after("zero", "two")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.next.record == "zero"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_insert_after_nonempty_match_tail():
+    """Should insert after on a non-empty list with a tail match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_after("zero", "three")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.next.record == "three"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "zero"
+    assert a.tail.next is None
+
+
+def test_should_insert_after_nonempty_no_match():
+    """Should insert after on a non-empty list without a match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_after("zero", "four")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.next.record == "three"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "zero"
+    assert a.tail.next is None
+
+
+def test_should_insert_after_none_empty():
+    """Should insert after no record with an empty list."""
+    a = DoublyLinkedList()
+    a.insert_after("one")
+
+    assert len(a) == 1
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.head == a.tail
+
+
+def test_should_insert_after_none_nonempty():
+    """Should insert after no record on a non-empty list."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_after("zero")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.next.record == "three"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "zero"
+    assert a.tail.next is None
+
+
+def test_should_insert_before_empty():
+    """Should insert before with an empty list."""
+    a = DoublyLinkedList()
+    a.insert_before("one", "zero")
+
+    assert len(a) == 1
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.head == a.tail
+
+
+def test_should_insert_before_nonempty_match_head():
+    """Should insert before on a non-empty list with a head match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_before("zero", "one")
+
+    assert len(a) == 4
+    assert a.head.record == "zero"
+    assert a.head.prev is None
+    assert a.head.next.record == "one"
+    assert a.head.next.next.record == "two"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_insert_before_nonempty_match_middle():
+    """Should insert before on a non-empty list with a middle match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_before("zero", "two")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "zero"
+    assert a.head.next.next.record == "two"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_insert_before_nonempty_match_tail():
+    """Should insert before on a non-empty list with a tail match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_before("zero", "three")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.next.record == "zero"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_insert_before_nonempty_no_match():
+    """Should insert before on a non-empty list without a match."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_before("zero", "four")
+
+    assert len(a) == 4
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.next.record == "three"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "zero"
+    assert a.tail.next is None
+
+
+def test_should_insert_before_none_empty():
+    """Should insert before no record with an empty list."""
+    a = DoublyLinkedList()
+    a.insert_before("one")
+
+    assert len(a) == 1
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.head == a.tail
+
+
+def test_should_insert_before_none_nonempty():
+    """Should insert before no record on a non-empty."""
+    a = DoublyLinkedList("one", "two", "three")
+
+    a.insert_before("zero")
+
+    assert len(a) == 4
+    assert a.head.record == "zero"
+    assert a.head.prev is None
+    assert a.head.next.record == "one"
+    assert a.head.next.next.record == "two"
+    assert a.head.next.next.next == a.tail
+    assert a.tail.prev.prev.prev == a.head
+    assert a.tail.record == "three"
+    assert a.tail.next is None
+
+
+def test_should_reverse_empty():
+    """Should reverse an empty list."""
+    a = DoublyLinkedList()
+    a.reverse()
+
+    assert len(a) == 0
+    assert a.head is None
+    assert a.tail is None
+    assert a.head == a.tail
+
+
+def test_should_reverse_one_item():
+    """Should reverse a one item list."""
+    a = DoublyLinkedList("one")
+    a.reverse()
+
+    assert len(a) == 1
+    assert a.head.record == "one"
+    assert a.head.prev is None
+    assert a.head.next is None
+    assert a.head == a.tail
+
+
+def test_should_reverse_many_items():
+    """Should reverse a many item list."""
+    a = DoublyLinkedList("one", "two", "three")
+    a.reverse()
+
+    assert len(a) == 3
+    assert a.head.record == "three"
+    assert a.head.prev is None
+    assert a.head.next.record == "two"
+    assert a.head.next.prev.record == "three"
+    assert a.tail == a.head.next.next
+    assert a.tail.record == "one"
+    assert a.tail.prev.record == "two"
+    assert a.tail.next is None
